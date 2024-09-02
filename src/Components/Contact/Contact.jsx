c404f7426891ff4996443fec831189b89efd31d7
Hamid Naveed <hamidnaveedch@gmail.com>
2024-09-02 21:16:20 +0500

    Send Contact Form data on Email

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,6 +6,34 @@ import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "94c997b9-fee3-400e-9f27-21d052c641f1");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        }
+        else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+        alert(result)
+    };
+
     return (
         <div id='contact' className='contact'>
             <div className="contact-title">
@@ -31,7 +59,7 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <form className="contact-right">
+                <form onSubmit={onSubmit} className="contact-right">
                     <label htmlFor="">Your Name</label>
                     <input type="text" placeholder='Enter your name' name='name' />
                     <label htmlFor="">Your Email</label>
@@ -39,7 +67,9 @@ const Contact = () => {
                     <label htmlFor="">Write your message here</label>
                     <textarea name="message" rows={8} placeholder='Enter your message'></textarea>
                     <button type='submit' className='contact-submit'>Submit now</button>
+                    <span>{result}</span>
                 </form>
+
             </div>
         </div>
     )
